c9b0ccc85ce7b4746c9f35687ad30fea3eef5294
anshika1709 <awasthi.anshika1709@gmail.com>
2021-01-26 15:06:42 +0530

    Added local location feature

--- script.js
+++ script.js
@@ -11,13 +11,35 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-//For Leaflet Library
-let map = L.map('map').setView([51.505, -0.09], 13);
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition((position) => {
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+        //My location
+        const { latitude, longitude } = position.coords;
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+        //For Leaflet Library
+        let map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+
+        //click event
+        map.on('click', (e) => {
+            const { lat, lng } = e.latlng;
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                    })
+                )
+                .setPopupContent(`workout`)
+                .openPopup();
+        })
+
+    }, () => { alert("Couldn't load your location") });
