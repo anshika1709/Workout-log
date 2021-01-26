83e1a173c631ac1003f3a682edd55ffe5ad5ab02
anshika1709 <awasthi.anshika1709@gmail.com>
2021-01-26 22:08:44 +0530

    Implemented encapsulation on methods

--- script.js
+++ script.js
@@ -11,14 +11,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition((position) => {
 
-        //My location
+let map;
+let loadEvent;
+class App {
+
+
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    };
+
+    _getPosition() {
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => { alert("Couldn't load your location") });
+    }
+
+    _loadMap(position) { //My location
         const { latitude, longitude } = position.coords;
 
         //For Leaflet Library
-        let map = L.map('map').setView([latitude, longitude], 13);
+        map = L.map('map').setView([latitude, longitude], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,20 +40,42 @@ if (navigator.geolocation)
 
 
         //click event
-        map.on('click', (e) => {
-            const { lat, lng } = e.latlng;
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(
-                    L.popup({
-                        maxWidth: 250,
-                        minWidth: 100,
-                        autoClose: false,
-                        closeOnClick: false,
-                        className: 'running-popup',
-                    })
-                )
-                .setPopupContent(`workout`)
-                .openPopup();
-        })
+        map.on('click', this._showForm.bind(this));
+    }
 
-    }, () => { alert("Couldn't load your location") });
+    _showForm(mapEvent) {
+        loadEvent = mapEvent;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+
+    }
+
+    _newWorkout(e) {
+
+        e.preventDefault();
+
+        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = " ";
+        const { lat, lng } = loadEvent.latlng;
+        L.marker([lat, lng]).addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                })
+            )
+            .setPopupContent(`workout`)
+            .openPopup();
+    }
+
+}
+
+let newApp = new App();
